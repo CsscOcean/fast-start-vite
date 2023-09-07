299651dfda2f688df58df82a20a06f9a34527d7f
huangyingwen <hyw.huangyingwen@gmail.com>
2023-09-07 15:05:33 +0800

    chore: api 接口生成 path 和 query 放在一个对象里面

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -2,9 +2,7 @@ module.exports = {
   '*.{ts,tsx,vue}': 'eslint --cache --fix',
   '*.{css,scss,less,vue,html}': 'stylelint --fix',
   '*.{ts,tsx,css,scss,less,md,vue,json}': async files => {
-    const filesToLint = files.filter(
-      a => !a.includes('auto-imports.d.ts') && !a.includes('./src/components.d.ts'),
-    );
+    const filesToLint = files.filter(a => !a.includes('auto-imports.d.ts') && !a.includes('components.d.ts'));
 
     if (!filesToLint.length) return 'echo';
 
