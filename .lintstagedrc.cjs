6a99434295814f6c08df5415f0d9c53a80431db8
huangyingwen <hyw.huangyingwen@gmail.com>
2023-09-07 15:04:58 +0800

    chore: prettier 忽略自动生成的类型文件

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,5 +1,13 @@
 module.exports = {
   '*.{ts,tsx,vue}': 'eslint --cache --fix',
   '*.{css,scss,less,vue,html}': 'stylelint --fix',
-  '*.{ts,tsx,css,scss,less,md,vue,json}': 'prettier --write',
+  '*.{ts,tsx,css,scss,less,md,vue,json}': async files => {
+    const filesToLint = files.filter(
+      a => !a.includes('auto-imports.d.ts') && !a.includes('./src/components.d.ts'),
+    );
+
+    if (!filesToLint.length) return 'echo';
+
+    return `prettier --write ${filesToLint.join(' ')}`;
+  },
 };
