75e52e9f5aaddd63ab8bea60031ef92f921af498
huangyingwen <hyw.huangyingwen@gmail.com>
2024-04-25 09:09:03 +0800

    feat: 组件换成 antd

--- vite.config.ts
+++ vite.config.ts
@@ -3,46 +3,31 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
-import postCssPxToRem from 'postcss-pxtorem';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 import svgLoader from 'vite-svg-loader';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    svgLoader(),
+    vueJsx(),
+    svgLoader({
+      svgoConfig: {
+        plugins: ['prefixIds'],
+      },
+    }),
     AutoImport({
       // Auto import functions from Vue, e.g. ref, reactive, toRef...
       // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
       imports: ['vue'],
-
-      // Auto import functions from Element Plus, e.g. ElMessage, ElMessageBox... (with style)
-      // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
-      resolvers: [ElementPlusResolver()],
-
       dts: path.resolve(__dirname, './src/auto-imports.d.ts'),
     }),
     Components({
-      resolvers: [
-        // Auto register Element Plus components
-        // 自动导入 Element Plus 组件
-        ElementPlusResolver(),
-      ],
-
+      resolvers: [AntDesignVueResolver({ importStyle: false })],
       dts: path.resolve(__dirname, './src/components.d.ts'),
     }),
   ],
-  css: {
-    postcss: {
-      plugins: [
-        postCssPxToRem({
-          rootValue: 192,
-          propList: ['*'],
-        }),
-      ],
-    },
-  },
   base: './',
   /**
    * 与“根”相关的目录，构建输出将放在其中。如果目录存在，它将在构建之前被删除。
@@ -50,9 +35,8 @@ export default defineConfig({
    */
   // outDir: 'dist',
   server: {
-    // hostname: '0.0.0.0',
-    host: 'localhost',
-    port: 3333,
+    host: '0.0.0.0',
+    port: 8080,
     // // 是否自动在浏览器打开
     // open: true,
     // // 是否开启 https
@@ -72,14 +56,6 @@ export default defineConfig({
     // 导入文件夹别名
     alias: {
       '@': path.resolve(__dirname, './src'),
-      views: path.resolve(__dirname, './src/views'),
-      components: path.resolve(__dirname, './src/components'),
-      utils: path.resolve(__dirname, './src/utils'),
-      less: path.resolve(__dirname, './src/less'),
-      assets: path.resolve(__dirname, './src/assets'),
-      com: path.resolve(__dirname, './src/components'),
-      store: path.resolve(__dirname, './src/store'),
-      mixins: path.resolve(__dirname, './src/mixins'),
     },
   },
 });
